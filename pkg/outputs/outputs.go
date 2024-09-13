1fc12c508739d49486d4cb31cafa6293a5364057
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-14 02:23:12 +0530

    converted to pulumi git repo

--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -1,11 +1,5 @@
 package outputs
 
-import (
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/awslambda"
-	"github.com/pulumi/pulumi/sdk/v3/go/auto"
+const (
+	OutputKey = "value"
 )
-
-func PulumiOutputsToStackOutputsConverter(pulumiOutputs auto.OutputMap,
-	input *awslambda.AwsLambdaStackInput) *awslambda.AwsLambdaStackOutputs {
-	return &awslambda.AwsLambdaStackOutputs{}
-}
